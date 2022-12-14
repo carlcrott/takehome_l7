604f070dfe0210ebd26a45c75cf1db6f3f3fe263
Carl Crott <carlcrott@gmail.com>
2022-12-14 11:07:15 -0800

    initial commit

---
+++ problem_1/question1.py
@@ -0,0 +1,71 @@
+
+
+
+"""
+
+
+docker build . 
+ddocker compose up
+
+
+
+"""
+
+
+
+"""
+#### Part 1
+>> ingest the attached example file
+
+So we need to sort out which columns which might be which.
+
+How complex is the total solution space?
+
+This could literally be endlessly complex.
+
+First off we should dump any errors -- so if were ingesting something that fails, well we have an example of WHAT failure happened.
+
+We should test one of the columsn to see if it matches against a known-true solution: EX: States explitic, and State abbreviated.
+
+Im not exactly sure what this data should be.
+
+"""
+
+
+
+
+"""
+
+#### Part 2
+>> validates Benford’s assertion based on the '7_2009' column in the supplied file
+
+I dont know what exactly this column is... so how are we going to detect for it... so I might need more data.
+
+It SAYS its a 'census' ...so we assume population?
+
+I suppose I could just graph all 3 and just roll the dice on everything?
+
+
+"""
+
+
+
+
+
+"""
+#### Part 3
+> graph of the observed distribution of numbers
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
